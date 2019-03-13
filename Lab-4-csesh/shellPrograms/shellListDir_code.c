f3037e9609677fd9cc86430f2ad7276c811b8527
Lee Gui An <guilee186@gmail.com>
2019-03-13 17:40:51 +0800

    some updates to the listdir code

--- Lab-4-csesh/shellPrograms/shellListDir_code.c
+++ Lab-4-csesh/shellPrograms/shellListDir_code.c
@@ -13,9 +13,25 @@
 	List the items in the directory
 */
 int shellListDir_code(char** args)
-{    
+{
+	struct dirent **dirList;
+	struct stat fileInfo;
+	char buffer[80];
+	int num_files = scandir(".", &dirList, filterHidden, sortFiles);
 	printf("Hello from shellListDir_code. Unfortunately, 'listdir' hasn't been implemented yet.");
     return 1;
+}
+
+int filterHidden(const struct dirent *entry) 
+{
+	// we need to filter out all of the files that ought to be hidden; or maybe we could ignore it entirely...?
+	return strncmp(entry->d_name, ".", 1);
+}
+
+int sortFiles(const struct dirent **a, const struct dirent **b)
+{
+	struct stat astat, bstat;
+
 }
 
 int main(int argc, char** args){
--- Lab-4-csesh/shellPrograms/shellPrograms.h
+++ Lab-4-csesh/shellPrograms/shellPrograms.h
@@ -9,6 +9,7 @@
 
 #include <sys/wait.h>
 #include <sys/types.h>
+#include <sys/stat.h>
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
