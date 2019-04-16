8bb62ad1b2ade4f3bcd72010989bc8d38f7a97bd
SolsticeDante <solsticedante@gmail.com>
2019-04-16 22:57:37 +0800

    Add theAuthentication() function for server and client (nth inside yet); Add my DIR for client/server public keys

--- ProgAssignment2/src/main/java/ClientSecured.java
+++ ProgAssignment2/src/main/java/ClientSecured.java
@@ -9,10 +9,11 @@ import java.security.PrivateKey;
 import java.security.PublicKey;
 
 public class ClientSecured {
-    static String filedir = "D:/Github/50-005-Labs/prog-assignment-2/";
+    // static String filedir = "D:/Github/50-005-Labs/prog-assignment-2/";
+    static String filedir = "/home/xubuntu/Desktop/50-005-Labs/prog-assignment-2/"  // for junde
     static String clientPublicKeyFile = "clientpublic.der";
     static String clientPrivateKeyFile = "clientkey.der";
-    
+
     final static int CP_1_PACKET = 501;
     final static int CP_2_PACKET = 502;
     final static int FILE_HEADER_PACKET = 0;
@@ -21,8 +22,7 @@ public class ClientSecured {
     final static int PUB_KEY_PACKET = 102;
     final static int SEND_SESSION_KEY = 200;
     final static int OK_PACKET = 80;
-    
-    
+
     // PACKET SCHEMA:
     /*
     0: TRANSFER FILE NAME
@@ -33,49 +33,49 @@ public class ClientSecured {
     501: SET MODE TO CP-1
     502: SET MODE TO CP-2
     */
-    
-    
+
+
     // Mode = 1 is CP-1;
     // Mode = 2 is CP-2;
-    
+
     private final static int MODE = 1;
     static RSAKeyPair clientKeys;
     static PublicKey serverKey;
     static Key sessionKey;
     static byte[] filebytes;
-    
+
     public static void main(String[] args) {
-        
+
         String filename = filedir + "rr.txt";
         if (args.length > 0) filename = args[0];
-        
+
         String serverAddress = "localhost";
         if (args.length > 1) filename = args[1];
-        
+
         int port = 4321;
         if (args.length > 2) port = Integer.parseInt(args[2]);
-        
+
         int numBytes = 0;
-        
+
         Socket clientSocket = null;
-        
+
         DataOutputStream toServer = null;
         DataInputStream fromServer = null;
-        
+
         FileInputStream fileInputStream = null;
         BufferedInputStream bufferedFileInputStream = null;
-        
+
         long timeStarted = System.nanoTime();
-        
+
         try {
-            
+
             System.out.println("Establishing connection to server...");
-            
+
             // Connect to server and get the input and output streams
             clientSocket = new Socket(serverAddress, port);
             toServer = new DataOutputStream(clientSocket.getOutputStream());
             fromServer = new DataInputStream(clientSocket.getInputStream());
-            
+
             // TODO: Request Encrypted Identity with Nonce
             // TODO: Store Reply Nonce
             // TODO: Store Identity
@@ -85,18 +85,18 @@ public class ClientSecured {
             // TODO: Open Signed Certificate and extract public key
             // TODO: Decrypt Message Digest and verify Message Digest
             // TODO: If all is well, continue; else, close socket
-            
+
             System.out.print("Initializing File Sending Process...");
-            
+
             if (MODE == 1) {
                 // DONE: CP-1 Style of Cryptography
                 // DONE: Reply with public key
                 // TODO: Encrypt file bytes with public key and then private key
                 // TODO: Send Encrypted bytes
-                
+
                 System.out.println("CP-1 Mode Detected... notifying server.");
                 toServer.writeInt(CP_1_PACKET);
-    
+
                 System.out.print("Retrieving Client keys...");
                 try {
                     clientKeys = new RSAKeyPair(filedir + clientPublicKeyFile,
@@ -106,10 +106,10 @@ public class ClientSecured {
                     System.out.println("Key Files not found!");
                 }
                 System.out.println("done.");
-                
+
                 PrivateKey privKey = clientKeys.getPrivateKey();
                 PublicKey pubKey = clientKeys.getPublicKey();
-                
+
                 System.out.println("Transmitting public key to Server...");
                 toServer.writeInt(PUB_KEY_PACKET);
                 byte[] bytePublicKey = pubKey.getEncoded();
@@ -119,7 +119,7 @@ public class ClientSecured {
                 System.out.println("CP-2 Mode Detected... notifying server.");
                 toServer.writeInt(CP_2_PACKET);
             }
-            
+
             // TODO: CP-2 Style of Cryptography
             // TODO: Generate Session Key
             // TODO: Encrypt Session Key w/ SERVER Public
@@ -127,19 +127,19 @@ public class ClientSecured {
             // TODO: Receive Encrypted Confirmation w/ reply message (encrypted with AES key)
             // TODO: Decrypt Confirmation with AES key, confirm message
             // TODO: Encrypt bytes with session key
-            
+
             System.out.print("Sending File now..");
             // Send the filename
             sendChunk(filename.getBytes(), toServer, FILE_HEADER_PACKET);
             //toServer.flush();
-            
+
             // Open the file
             fileInputStream = new FileInputStream(filename);
             bufferedFileInputStream = new BufferedInputStream(fileInputStream);
-            
+
             byte[] fromFileBuffer = new byte[117];
             MessageDigest md = MessageDigest.getInstance("SHA-1");
-            
+
             // Send the file
             for (boolean fileEnded = false; !fileEnded; ) {
                 numBytes = bufferedFileInputStream.read(fromFileBuffer);
@@ -149,14 +149,14 @@ public class ClientSecured {
                 toServer.flush();
                 System.out.print(".");
             }
-            
+
             System.out.println("done.");
             System.out.print("Sending Digest to Verify.");
-            
+
             // Send Digest to Check
             byte[] digest = md.digest();
             sendChunk(digest, toServer, FILE_DIGEST_PACKET);
-            
+
             int reply = fromServer.readInt();
             if (reply == OK_PACKET){
                 System.out.println("Success.");
@@ -164,17 +164,17 @@ public class ClientSecured {
                 fileInputStream.close();
             }
             System.out.println();
-            
+
             System.out.println("Closing connection...");
-            
+
         } catch (Exception e) {
             e.printStackTrace();
         }
-        
+
         long timeTaken = System.nanoTime() - timeStarted;
         System.out.println("Program took: " + timeTaken / 1000000.0 + "ms to run");
     }
-    
+
     static void sendChunk(byte[] bytes, DataOutputStream toServer, int packet_type) throws Exception{
         toServer.writeInt(packet_type);
         byte[] privateEncoded, bytesEncrypted;
@@ -185,10 +185,21 @@ public class ClientSecured {
         }
         if (MODE == 2){
             // TODO
-            
+
         }
-        
+
         toServer.writeInt(bytesEncrypted.length);
         toServer.write(bytesEncrypted);
+    }
+
+    static void theAuthentication(){
+        /**
+        This function will be called in tandem with the theAuthentication() function in server.
+        Both enters function, do appropriate authentication procedures and exit their respective functions together.
+
+        1. Client says hello to server
+        2. 
+
+        **/
     }
 }
--- ProgAssignment2/src/main/java/ServerSecured.java
+++ ProgAssignment2/src/main/java/ServerSecured.java
@@ -9,10 +9,11 @@ import java.security.PublicKey;
 import java.security.spec.X509EncodedKeySpec;
 
 public class ServerSecured {
-    static String filedir = "D:/Github/50-005-Labs/prog-assignment-2/";
+    // static String filedir = "D:/Github/50-005-Labs/prog-assignment-2/";
+    static String filedir = "/home/xubuntu/Desktop/50-005-Labs/prog-assignment-2/"  // for junde
     static String serverPublicKeyFile = "example.org.public.der";
     static String serverPrivateKeyFile = "example.org.private.der";
-    
+
     final static int CP_1_PACKET = 501;
     final static int CP_2_PACKET = 502;
     final static int FILE_HEADER_PACKET = 0;
@@ -20,7 +21,7 @@ public class ServerSecured {
     final static int FILE_DIGEST_PACKET = 2;
     final static int PUB_KEY_PACKET = 102;
     final static int SEND_SESSION_KEY = 200;
-    
+
     // Note:
     // Mode = 1 is CP-1;
     // Mode = 2 is CP-2;
@@ -28,7 +29,7 @@ public class ServerSecured {
     private static boolean modeHasBeenSet = false;
     private static PublicKey clientKey;
     private static RSAKeyPair serverKeys;
-    
+
     public static void main(String[] args) {
         System.out.println("Starting up Server...");
         System.out.print("Retrieving Keys...");
@@ -39,33 +40,33 @@ public class ServerSecured {
 //            e.printStackTrace();
         }
         System.out.println("done.");
-        
-        
+
+
         int port = 4321;
         if (args.length > 0) port = Integer.parseInt(args[0]);
-        
+
         ServerSocket welcomeSocket = null;
         Socket connectionSocket = null;
         DataOutputStream toClient = null;
         DataInputStream fromClient = null;
-        
+
         FileOutputStream fileOutputStream = null;
         BufferedOutputStream bufferedFileOutputStream = null;
-        
+
         try {
             welcomeSocket = new ServerSocket(port);
             connectionSocket = welcomeSocket.accept();
             fromClient = new DataInputStream(connectionSocket.getInputStream());
             toClient = new DataOutputStream(connectionSocket.getOutputStream());
-            
+
             while (!connectionSocket.isClosed()) {
-                
+
                 int packetType = fromClient.readInt();
-                
+
                 // TODO: Read Request Packet from Client, use as Nonce
                 // TODO: Reply with Nonce and Encrypted Identity
                 // TODO: Read Request for Signed Certificate, Encrypt Digest of Cert
-                
+
                 // set MODE of cryptography for uploading
                 if (packetType == CP_1_PACKET && !modeHasBeenSet) {
                     modeHasBeenSet = true;
@@ -75,47 +76,46 @@ public class ServerSecured {
                     modeHasBeenSet = true;
                     mode = 2;
                 }
-                
+
                 // Inbound Public Key Packet
                 if (packetType == PUB_KEY_PACKET) {
                     System.out.print("Receiving public key from client...");
                     byte[] clientPublicKeyBytes = new byte[128];
                     fromClient.readFully(clientPublicKeyBytes);
-                    
+
                     KeyFactory pubkf = KeyFactory.getInstance("RSA");
                     X509EncodedKeySpec clientKeySpec = new X509EncodedKeySpec(clientPublicKeyBytes);
                     clientKey = pubkf.generatePublic(clientKeySpec);
-                    
-                    
+
                 }
-                
+
                 // If the packet is for transferring the filename
                 if (packetType == 0) {
-                    
+
                     System.out.println("Receiving file...");
-                    
+
                     int numBytes = fromClient.readInt();
                     byte[] filename = new byte[numBytes];
                     // Must use read fully!
                     // See: https://stackoverflow.com/questions/25897627/datainputstream-read-vs-datainputstream-readfully
                     fromClient.readFully(filename, 0, numBytes);
-                    
+
                     fileOutputStream = new FileOutputStream("recv_" + new String(filename, 0, numBytes));
                     bufferedFileOutputStream = new BufferedOutputStream(fileOutputStream);
-                    
+
                     // If the packet is for transferring a chunk of the file
                 } else if (packetType == 1) {
-                    
+
                     int numBytes = fromClient.readInt();
                     byte[] block = new byte[numBytes];
                     fromClient.readFully(block, 0, numBytes);
-                    
+
                     if (numBytes > 0)
                         bufferedFileOutputStream.write(block, 0, numBytes);
-                    
+
                     if (numBytes < 117) {
                         System.out.println("Closing connection...");
-                        
+
                         if (bufferedFileOutputStream != null) bufferedFileOutputStream.close();
                         if (bufferedFileOutputStream != null) fileOutputStream.close();
                         fromClient.close();
@@ -124,16 +124,21 @@ public class ServerSecured {
                         modeHasBeenSet = false;
                     }
                 }
-                
+
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
-        
+
     }
-    
+
     static byte[] decryptChunk(byte[] encrypted){
         //TODO
     }
-    
+    static void theAuthentication(){
+        /**
+        This function will be called in tandem with the theAuthentication() function in client.
+        Both enters function, do appropriate authentication procedures and exit their respective functions together.
+        **/
+    }
 }
